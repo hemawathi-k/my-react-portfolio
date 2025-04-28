2a4375d302399fa42e7e67f0d6464ae735f3a210
Hemawathi Karthikeyan <hemawathi2002@gmail.com>
2025-04-28 21:51:03 +0530

    commit

--- src/About.jsx
+++ src/About.jsx
@@ -1,22 +1,24 @@
 import './About.css';
-import cartoon from './assets/cartoon.png'; // import cartoon image
-import resume from './assets/Resume Updated.pdf'; // import resume pdf
+
+// Import the image at the top of your file
+import cartoonImage from '../assets/cartoon.png';
 
 export default function About() {
-    return (
-        <>
-         <div className="about-layout" id='about'>
-            <div className='img-div'>
-              <img src={cartoon} alt="Cartoon" />  {/* Use imported cartoon */}
-            </div>
-            <div className='right-content'>
-                <h1 id='h1'>About Me</h1>
-                <p>
-                I'm Hemawathi Karthikeyan, a B.E. Computer Science graduate with a strong interest in Full Stack React Development. I'm passionate about building dynamic, responsive web applications and constantly learning new technologies.
-                </p>
-                <a href={resume} download className="download-btn1">Resume</a> {/* Use imported resume */}
-            </div>
-         </div>
-        </>
-    )
+  return (
+    <>
+      <div className="about-layout" id='about'>
+        <div className='img-div'>
+          {/* Use the imported image here */}
+          <img src={cartoonImage} alt="Cartoon" />
+        </div>
+        <div className='right-content'>
+          <h1 id='h1'>About Me</h1>
+          <p>
+            I'm Hemawathi Karthikeyan, a B.E. Computer Science graduate with a strong interest in Full Stack React Development. I'm passionate about building dynamic, responsive web applications and constantly learning new technologies.
+          </p>
+          <a href="./assets/Resume Updated.pdf" download className="download-btn1">Resume</a>
+        </div>
+      </div>
+    </>
+  );
 }
