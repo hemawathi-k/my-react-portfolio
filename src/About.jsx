ce06e361a6d28e0fbf3d7ba1c92d98be3c1370f7
Hemawathi Karthikeyan <hemawathi2002@gmail.com>
2025-04-28 21:45:01 +0530

    img path edit

--- src/About.jsx
+++ src/About.jsx
@@ -1,19 +1,22 @@
-import './About.css'
-export default function About(){
-    return(
+import './About.css';
+import cartoon from './assets/cartoon.png'; // import cartoon image
+import resume from './assets/Resume Updated.pdf'; // import resume pdf
+
+export default function About() {
+    return (
         <>
          <div className="about-layout" id='about'>
             <div className='img-div'>
-              <img src="./assets/cartoon.png"/>
+              <img src={cartoon} alt="Cartoon" />  {/* Use imported cartoon */}
             </div>
             <div className='right-content'>
                 <h1 id='h1'>About Me</h1>
                 <p>
                 I'm Hemawathi Karthikeyan, a B.E. Computer Science graduate with a strong interest in Full Stack React Development. I'm passionate about building dynamic, responsive web applications and constantly learning new technologies.
                 </p>
-                <a href="./assets/Resume Updated.pdf" download class="download-btn1">Resume</a>
+                <a href={resume} download className="download-btn1">Resume</a> {/* Use imported resume */}
             </div>
          </div>
         </>
     )
-} 
+}
--- src/Projects.jsx
+++ src/Projects.jsx
@@ -1,26 +1,45 @@
 import React from 'react';
 import './Projects.css';
 
+import img1 from '../assets/1.png';
+import img2 from '../assets/2.png';
+import img3 from '../assets/3.png';
+import img4 from '../assets/4.png';
+import img5 from '../assets/5.png';
+import img6 from '../assets/6.png';
+import img7 from '../assets/7.png';
+import img8 from '../assets/8.png';
+import img9 from '../assets/9.png';
+import img10 from '../assets/10.png';
+import img11 from '../assets/11.png';
+import img12 from '../assets/12.png';
+import img13 from '../assets/13.png';
+import img14 from '../assets/14.png';
+import img15 from '../assets/15.png';
+import img16 from '../assets/16.png';
+import img17 from '../assets/17.png';
+import img18 from '../assets/18.png';
+
 const Projects = () => {
   const projects = [
-    { img: '../assets/1.png' },
-    { img: '../assets/2.png' },
-    { img: '../assets/3.png' },
-    { img: '../assets/4.png' },
-    { img: '../assets/5.png' },
-    { img: '../assets/6.png' },
-    { img: '../assets/7.png' },
-    { img: '../assets/8.png' },
-    { img: '../assets/9.png' },
-    { img: '../assets/10.png' },
-    { img: '../assets/11.png' },
-    { img: '../assets/12.png' },
-    { img: '../assets/13.png' },
-    { img: '../assets/14.png' },
-    { img: '../assets/15.png' },
-    { img: '../assets/16.png' },
-    { img: '../assets/17.png' },
-    { img: '../assets/18.png' },
+    { img: img1 },
+    { img: img2 },
+    { img: img3 },
+    { img: img4 },
+    { img: img5 },
+    { img: img6 },
+    { img: img7 },
+    { img: img8 },
+    { img: img9 },
+    { img: img10 },
+    { img: img11 },
+    { img: img12 },
+    { img: img13 },
+    { img: img14 },
+    { img: img15 },
+    { img: img16 },
+    { img: img17 },
+    { img: img18 },
   ];
 
   return (
